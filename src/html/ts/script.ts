6b1afea33001df8cdfe114be90c031aa71198463
nnnnusui <nnnnusui@gmail.com>
2021-01-13 21:06:34 +0900

    add cellクリック時に座標をlog出力

--- src/html/ts/script.ts
+++ src/html/ts/script.ts
@@ -6,4 +6,21 @@ pianoroll.style.setProperty("--column", `${column}`);
 pianoroll.style.setProperty("--row", `${row}`);
 
 const background = pianoroll.getElementsByClassName("background")[0] as HTMLElement;
-[...Array(column * row).keys()].forEach(index=> background.appendChild(document.createElement('div')))
+const background_cell = document.createElement('div') as HTMLElement;
+[...Array(column * row).keys()]
+    .map(_ => {
+        const cell = background_cell.cloneNode() as HTMLElement;
+        cell.addEventListener("click", event => {
+            const target = event.target as HTMLElement;
+            const parent = target.parentElement as HTMLElement;
+            const index = Array.from(parent.children).indexOf(target);
+            
+            const rowIndex = index / column;
+            const columnIndex = index % column;
+            console.log(`index: ${index}, row: ${rowIndex}, column: ${columnIndex}`);
+            // ここにNote追加処理
+        });
+        return cell;
+    })
+    .forEach(it => background.appendChild(it))
+const test = {...background_cell};

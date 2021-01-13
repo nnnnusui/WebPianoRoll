5b973039ccb20c65c217d222f4dcf7d8fb4d43c9
nnnnusui <nnnnusui@gmail.com>
2021-01-13 22:48:40 +0900

    add 拡大縮小

--- src/html/ts/script.ts
+++ src/html/ts/script.ts
@@ -1,9 +1,13 @@
 
 const pianoroll = document.getElementsByClassName("pianoroll")[0] as HTMLElement;
-const column = 25;
 const row = 50;
-pianoroll.style.setProperty("--column", `${column}`);
+const column = 25;
+let vertical_magnification = 0;
+let horizontal_magnification = 0;
 pianoroll.style.setProperty("--row", `${row}`);
+pianoroll.style.setProperty("--column", `${column}`);
+pianoroll.style.setProperty("--vertical-magnification", `${vertical_magnification}`);
+pianoroll.style.setProperty("--horizontal-magnification", `${horizontal_magnification}`);
 
 const note_container = document.createElement('section') as HTMLElement;
 note_container.setAttribute("class", "note-container");
@@ -31,4 +35,29 @@ const background_cell_template = document.createElement('div') as HTMLElement;
         return cell;
     })
     .forEach(it => background.appendChild(it))
+
+
+let ctrl = false;
+document.addEventListener("keyup", event => ctrl = event.ctrlKey);
+document.addEventListener("keydown", event => ctrl = event.ctrlKey);
+window.addEventListener("mousewheel", event => ctrl ? event.preventDefault() : {}, { passive: false })
+pianoroll.onwheel = (event: WheelEvent) => {
+    if (!event.ctrlKey) return;
+    const plusOrMinusOrZero = (it: number) => {
+        if (it < 0) return -1;
+        if (it > 0) return  1;
+        return 0;
+    }
+    const y = plusOrMinusOrZero(event.deltaY);
+    const x = plusOrMinusOrZero(event.deltaX);
+    const after_vertical = vertical_magnification + y;
+    const after_horizontal = horizontal_magnification + x;
+
+    if (after_vertical >= 0)
+        vertical_magnification = after_vertical;
+    if (after_horizontal >= 0)
+        horizontal_magnification = after_horizontal;
+    pianoroll.style.setProperty("--vertical-magnification", `${vertical_magnification}`);
+    pianoroll.style.setProperty("--horizontal-magnification", `${horizontal_magnification}`);
+}
 

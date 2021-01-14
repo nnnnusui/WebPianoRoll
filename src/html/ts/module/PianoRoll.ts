72a2427702bc86ea6a779cf8a30b714996c0d7eb
nnnnusui <nnnnusui@gmail.com>
2021-01-14 18:11:07 +0900

    backup

--- src/html/ts/module/PianoRoll.ts
+++ src/html/ts/module/PianoRoll.ts
@@ -2,56 +2,37 @@ import { CssProperty } from "./CssProperty.js";
 import { Background } from "./pianoroll/Background.js";
 
 export class PianoRoll {
+    private readonly length: number;
+
+    private constructor(length: number) {
+        this.length = length;
+    }
+}
+class Element {
     static class_name: string = "pianoroll";
-    static generateTo(parent: HTMLElement, row_size: number, column_size: number): PianoRoll {
+    private readonly piano_roll: PianoRoll;
+    constructor(piano_roll: PianoRoll) {
+        this.piano_roll = piano_roll;
+    }
+    generate(): HTMLElement {
         const element = document.createElement('article') as HTMLElement;
-        parent.appendChild(element);
-        element.setAttribute("class", this.class_name);
-        element.style.setProperty("--row", `${row_size}`);
-        element.style.setProperty("--column", `${column_size}`);
+        element.setAttribute("class", Element.class_name);
+        [...Array(12).keys()]
+            .map(_ => document.createElement('div'))
+            .forEach(it => element.appendChild(it));
+        return element;
+    }
+    // static generateTo(parent: HTMLElement, row_size: number, column_size: number): PianoRoll {
+    //     parent.appendChild(element);
+    //     element.style.setProperty("--row", `${row_size}`);
+    //     element.style.setProperty("--column", `${column_size}`);
 
-        const piano_roll = new PianoRoll(element);
-        piano_roll.row = row_size;
-        piano_roll.column = column_size;
-        const background = Background.generateTo(element, piano_roll);
-        element.appendChild(background.element);
-        return piano_roll;
-    }
-    static fromParent(parent: HTMLElement): PianoRoll {
-        const element = parent.getElementsByClassName(this.class_name)[0] as HTMLElement;
-        return this.fromElement(element);
-    }
-    static fromElement(element: HTMLElement): PianoRoll {
-        const background = Background.fromParent(element);
-        return new PianoRoll(element);
-    }
-    readonly element: HTMLElement;
-    private readonly _row: CssProperty<number>;
-    set row(value: number) { this._row.set(value) }
-    private readonly _column: CssProperty<number>;
-    set column(value: number) { this._column.set(value) }
-    private readonly _vertical_magnification: CssProperty<number>;
-    set vertical_magnification(value: number) { this._vertical_magnification.set(value); }
-    private readonly _horizontal_magnification: CssProperty<number>;
-    set horizontal_magnification(value: number) { this._horizontal_magnification.set(value); }
+    //     const piano_roll = new PianoRoll(element);
+    //     piano_roll.row = row_size;
+    //     piano_roll.column = column_size;
+    //     const background = Background.generateTo(element, piano_roll);
+    //     element.appendChild(background.element);
+    //     return piano_roll;
+    // }
 
-    // readonly background: Background;
-
-    private constructor(element: HTMLElement) {
-        this.element = element;
-
-        this._row = CssProperty.fromComputed(element, "--row", CssProperty.toNumber);
-        this._column = CssProperty.fromComputed(element, "--column", CssProperty.toNumber);
-        this._vertical_magnification = CssProperty.fromComputed(element, "--vertical-magnification", CssProperty.toNumber);
-        this._horizontal_magnification = CssProperty.fromComputed(element, "--horizontal-magnification", CssProperty.toNumber);
-    }
-    put_note(index: number) {
-        const rowIndex = Math.floor(index / this.column) + 1;
-        const columnIndex = index % this.column + 1;
-        console.log(`row: ${rowIndex}, column: ${columnIndex}`);
-    //     const note = note_template.cloneNode() as HTMLElement;
-    //     note.style.setProperty("grid-row", `${rowIndex}`);
-    //     note.style.setProperty("grid-column", `${columnIndex}`);
-    //     note_container.appendChild(note);
-    }
 }
--- src/html/ts/script.ts
+++ src/html/ts/script.ts
@@ -1,68 +1,32 @@
-import { PianoRoll } from "./module/PianoRoll.js";
 
-const pianoRoll = PianoRoll.generateTo(document.body, 50, 25);
-console.log(`roll: ${pianoRoll.element}`);
+class Elm {
+    pianoroll() {
+        const element = document.createElement('article') as HTMLElement;
+        element.setAttribute("class", "pianoroll");
+        element.appendChild(this.background());
+        return element;
+    }
+    background() {
+        const element = document.createElement('div') as HTMLElement;
+        element.setAttribute("class", "background");
+        [...Array(1).keys()]
+            .map(_ => this.octave())
+            .forEach(it => element.appendChild(it));
+        return element;
+    }
+    octave() {
+        const element = document.createElement('div') as HTMLElement;
+        element.setAttribute("class", "octave");
+        [...Array(12).keys()]
+            .map(_ => this.line())
+            .forEach(it => element.appendChild(it));
+        return element;
+    }
+    line(): HTMLElement {
+        const element = document.createElement('div') as HTMLElement;
+        element.setAttribute("class", "line");
+        return element;
+    }
+}
 
-
-// const pianoroll = document.getElementsByClassName("pianoroll")[0] as HTMLElement;
-// const row = 50;
-// const column = 25;
-// let vertical_magnification = 0;
-// let horizontal_magnification = 0;
-// pianoroll.style.setProperty("--row", `${row}`);
-// pianoroll.style.setProperty("--column", `${column}`);
-// pianoroll.style.setProperty("--vertical-magnification", `${vertical_magnification}`);
-// pianoroll.style.setProperty("--horizontal-magnification", `${horizontal_magnification}`);
-
-// const note_container = document.createElement('section') as HTMLElement;
-// note_container.setAttribute("class", "note-container");
-// pianoroll.appendChild(note_container);
-// const note_template = document.createElement('div') as HTMLElement;
-
-// const background = pianoroll.getElementsByClassName("background")[0] as HTMLElement;
-// const background_cell_template = document.createElement('div') as HTMLElement;
-// [...Array(column * row).keys()]
-//     .map(_ => {
-//         const cell = background_cell_template.cloneNode() as HTMLElement;
-//         cell.addEventListener("click", event => {
-//             const target = event.target as HTMLElement;
-//             const parent = target.parentElement as HTMLElement;
-//             const index = Array.from(parent.children).indexOf(target);
-            
-//             const rowIndex = Math.floor(index / column) + 1;
-//             const columnIndex = index % column + 1;
-//             console.log(`index: ${index}, row: ${rowIndex}, column: ${columnIndex}`);
-//             const note = note_template.cloneNode() as HTMLElement;
-//             note.style.setProperty("grid-row", `${rowIndex}`);
-//             note.style.setProperty("grid-column", `${columnIndex}`);
-//             note_container.appendChild(note);
-//         });
-//         return cell;
-//     })
-//     .forEach(it => background.appendChild(it))
-
-
-// let ctrl = false;
-// document.addEventListener("keyup", event => ctrl = event.ctrlKey);
-// document.addEventListener("keydown", event => ctrl = event.ctrlKey);
-// window.addEventListener("mousewheel", event => ctrl ? event.preventDefault() : {}, { passive: false })
-// pianoroll.onwheel = (event: WheelEvent) => {
-//     if (!event.ctrlKey) return;
-//     const plusOrMinusOrZero = (it: number) => {
-//         if (it < 0) return -1;
-//         if (it > 0) return  1;
-//         return 0;
-//     }
-//     const y = plusOrMinusOrZero(event.deltaY);
-//     const x = plusOrMinusOrZero(event.deltaX);
-//     const after_vertical = vertical_magnification + y;
-//     const after_horizontal = horizontal_magnification + x;
-
-//     if (after_vertical >= 0)
-//         vertical_magnification = after_vertical;
-//     if (after_horizontal >= 0)
-//         horizontal_magnification = after_horizontal;
-//     pianoroll.style.setProperty("--vertical-magnification", `${vertical_magnification}`);
-//     pianoroll.style.setProperty("--horizontal-magnification", `${horizontal_magnification}`);
-// }
-
+document.body.appendChild(new Elm().pianoroll())

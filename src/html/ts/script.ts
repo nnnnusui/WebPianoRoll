e36aa302f00632e09fa3c830683b1d0f0c4ea315
nnnnusui <nnnnusui@gmail.com>
2021-01-15 11:57:41 +0900

    リセット 設計変更

--- src/html/ts/script.ts
+++ src/html/ts/script.ts
@@ -3,13 +3,25 @@ class Elm {
     pianoroll() {
         const element = document.createElement('article') as HTMLElement;
         element.setAttribute("class", "pianoroll");
+        element.appendChild(this.header());
+        element.appendChild(this.contents());
+        return element;
+    }
+    header() {
+        const element = document.createElement('div') as HTMLElement;
+        element.setAttribute("class", "timeline-header");
+        return element;
+    }
+    contents() {
+        const element = document.createElement('div') as HTMLElement;
+        element.setAttribute("class", "contents");
         element.appendChild(this.background());
         return element;
     }
     background() {
         const element = document.createElement('div') as HTMLElement;
         element.setAttribute("class", "background");
-        [...Array(1).keys()]
+        [...Array(3).keys()]
             .map(_ => this.octave())
             .forEach(it => element.appendChild(it));
         return element;

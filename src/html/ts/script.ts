f68efa26908c377343227666dd81501ec12895ca
nnnnusui <nnnnusui@gmail.com>
2021-01-16 13:37:11 +0900

    add background

--- src/html/ts/script.ts
+++ src/html/ts/script.ts
@@ -1,8 +1,14 @@
 
 class Elm {
+    readonly element: HTMLElement
     constructor(public length: number, public octave_size: number = 3) {
-
+        this.element = this.pianoroll()
     }
+
+    put_note(source: HTMLElement) {
+        console.log(source)
+    }
+
     pianoroll() {
         const element = document.createElement('article') as HTMLElement;
         element.setAttribute("class", "pianoroll");
@@ -13,7 +19,9 @@ class Elm {
     header() {
         const element = document.createElement('div') as HTMLElement;
         element.setAttribute("class", "timeline-header");
-        element.appendChild(this.bar_header());
+        [...Array(this.length).keys()]
+            .map(_ => this.bar_header())
+            .forEach(it => element.appendChild(it));
         return element;
     }
     bar_header() {
@@ -24,10 +32,20 @@ class Elm {
             .forEach(it => element.appendChild(it));
         return element;
     }
+    
     contents() {
         const element = document.createElement('div') as HTMLElement;
         element.setAttribute("class", "contents");
-        element.appendChild(this.note_container());
+        element.appendChild(this.background());
+        // element.appendChild(this.note_container());
+        return element;
+    }
+    background() {
+        const element = document.createElement('div') as HTMLElement;
+        element.setAttribute("class", "background");
+        [...Array(this.length).keys()]
+            .map(_ => this.bar())
+            .forEach(it => element.appendChild(it));
         return element;
     }
     note_container() {
@@ -39,31 +57,31 @@ class Elm {
         return element;
     }
 
-    bar() {
+    bar(row: number) {
         const element = document.createElement('div') as HTMLElement;
         element.setAttribute("class", "bar");
         [...Array(this.length).keys()]
-            .map(_ => this.time_index_cell())
+            .map(index => this.time_index_cell(row * (index + 1)))
             .forEach(it => element.appendChild(it));
         return element;
     }
-    time_index_cell() {
+    time_index_cell(row: number) {
         const element = document.createElement('div') as HTMLElement;
         element.setAttribute("class", "time-index-cell");
         [...Array(this.octave_size).keys()]
-            .map(_ => this.octave())
+            .map(index => this.octave(row, index))
             .forEach(it => element.appendChild(it));
         return element;
     }
-    octave() {
+    octave(row: number, column: number) {
         const element = document.createElement('div') as HTMLElement;
         element.setAttribute("class", "octave");
         [...Array(12).keys()]
-            .map(_ => this.cell())
+            .map(index => this.cell(row, column * (index + 1)))
             .forEach(it => element.appendChild(it));
         return element;
     }
-    cell(): HTMLElement {
+    cell(row: number, column: number): HTMLElement {
         const element = document.createElement('div') as HTMLElement;
         element.classList.add("cell");
         element.oncontextmenu = _ => false;
@@ -75,11 +93,11 @@ class Elm {
         const right_click = 2;
         const target = event.target as HTMLElement;
         switch(event.button) {
-            case left_click: target.classList.add("puted");break;
+            case left_click: this.put_note(target); break;//target.classList.add("puted");break;
             case right_click: target.classList.remove("puted");break;
         }
         return false;
     }
 }
 
-document.body.appendChild(new Elm(4).pianoroll())
+document.body.appendChild(new Elm(4).element)

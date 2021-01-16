0757a1518cda3cdae6545e917f2acefb669e64aa
nnnnusui <nnnnusui@gmail.com>
2021-01-16 16:05:04 +0900

    構造変更

--- src/html/ts/script.ts
+++ src/html/ts/script.ts
@@ -1,120 +1,11 @@
-
-class Contents {
-    background: Background
-    constructor(private piano_roll: PianoRoll) {
-        this.background = new Background(this.piano_roll)
-    }
-    element() {
-        const element = document.createElement('div') as HTMLElement;
-        element.setAttribute("class", "contents");
-        element.appendChild(this.background.element());
-        // element.appendChild(this.note_container());
-        return element;
-    }
-}
-class Background {
-    bars: Bar[]
-    constructor(private piano_roll: PianoRoll) {
-        this.bars = 
-            [...Array(this.piano_roll.length).keys()]
-                .map(_ => new Bar(this.piano_roll));
-    }
-    element() {
-        const element = document.createElement('div') as HTMLElement;
-        element.setAttribute("class", "background");
-        this.bars
-            .map(it => it.element())
-            .forEach(it => element.appendChild(it));
-        return element;
-    }
-}
-class Bar {
-    time_index_cells: TimeIndexCell[]
-    constructor(private piano_roll: PianoRoll) {
-        this.time_index_cells = 
-            [...Array(4).keys()]
-                .map(_ => new TimeIndexCell(this.piano_roll));
-    }
-    element() {
-        const element = document.createElement('div') as HTMLElement;
-        element.setAttribute("class", "bar");
-        this.time_index_cells
-            .map(it => it.element())
-            .forEach(it => element.appendChild(it));
-        return element;
-    }
-}
-class TimeIndexCell {
-    octaves: Octave[]
-    constructor(private piano_roll: PianoRoll) {
-        this.octaves =
-            [...Array(3).keys()]
-                .map(_ => new Octave(this.piano_roll));
-    }
-    element() {
-        const element = document.createElement('div') as HTMLElement;
-        element.setAttribute("class", "time-index-cell");
-        this.octaves
-            .map(it => it.element())
-            .forEach(it => element.appendChild(it));
-        return element;
-    }
-}
-class Octave {
-    cells: Cell[]
-    constructor(private piano_roll: PianoRoll) {
-        this.cells = 
-            [...Array(12).keys()]
-                .map(_ => new Cell(this.piano_roll));
-    }
-    element() {
-        const element = document.createElement('div') as HTMLElement;
-        element.setAttribute("class", "octave");
-        this.cells
-            .map(it => it.element())
-            .forEach(it => element.appendChild(it));
-        return element;
-    }
-}
-class Cell {
-    constructor(private piano_roll: PianoRoll) {}
-    element() {
-        const element = document.createElement('div') as HTMLElement;
-        element.classList.add("cell");
-        element.oncontextmenu = _ => false;
-        element.onmousedown = this.onclick.bind(this);
-        return element;
-    }
-    private onclick(event: MouseEvent) {
-        const left_click = 0;
-        const right_click = 2;
-        const target = event.target as HTMLElement;
-        switch(event.button) {
-            case left_click: this.piano_roll.put_note(this); break;
-            case right_click: target.classList.remove("puted");break;
-        }
-        return false;
-    }
-}
 class PianoRoll {
-    public beat: number = 4
-    public contents: Contents
-    constructor(public length: number, public octave_size: number = 3) {
-        this.contents = new Contents(this)
-    }
-
-    put_note(from: Cell) {
-        this.contents
-            .background
-            .bars
-        console.log(from)
-    }
+    constructor(public length: number, public octave_size: number = 3) {}
 
     element() {
         const element = document.createElement('article') as HTMLElement;
         element.setAttribute("class", "pianoroll");
         element.appendChild(this.header());
-        element.appendChild(this.contents.element());
+        element.appendChild(this.note_container());
         return element;
     }
     header() {
@@ -134,14 +25,6 @@ class PianoRoll {
         return element;
     }
     
-    background() {
-        const element = document.createElement('div') as HTMLElement;
-        element.setAttribute("class", "background");
-        [...Array(this.length).keys()]
-            .map(_ => this.bar())
-            .forEach(it => element.appendChild(it));
-        return element;
-    }
     note_container() {
         const element = document.createElement('div') as HTMLElement;
         element.setAttribute("class", "note-container");
@@ -150,7 +33,6 @@ class PianoRoll {
             .forEach(it => element.appendChild(it));
         return element;
     }
-
     bar() {
         const element = document.createElement('div') as HTMLElement;
         element.setAttribute("class", "bar");
@@ -178,21 +60,27 @@ class PianoRoll {
     cell(): HTMLElement {
         const element = document.createElement('div') as HTMLElement;
         element.classList.add("cell");
-        element.oncontextmenu = _ => false;
-        element.onmousedown = this.onclick.bind(this);
+        element.appendChild(this.background());
         return element;
     }
-    private onclick(event: MouseEvent) {
-        const left_click = 0;
-        const right_click = 2;
-        const target = event.target as HTMLElement;
-        switch(event.button) {
-            // case left_click: this.parent.put_note(this.time_index, this.pitch_index); break;//target.classList.add("puted");break;
-            case right_click: target.classList.remove("puted");break;
+    background(): HTMLElement {
+        const element = document.createElement('div') as HTMLElement;
+        element.classList.add("background");
+        element.onclick = event => {
+            const target = event.target as HTMLElement;
+            target.parentElement?.appendChild(this.note());
         }
-        return false;
+        return element;
+    }
+    note(): HTMLElement {
+        const element = document.createElement('div') as HTMLElement;
+        element.classList.add("note");
+        element.onclick = event => {
+            const target = event.target as HTMLElement;
+            target.remove();
+        }
+        return element;
     }
 }
-
 
 document.body.appendChild(new PianoRoll(4).element())

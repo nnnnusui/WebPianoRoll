9c20a47be5b088d6929c6ec7cd3ad1b523a1ccea
nnnnusui <nnnnusui@gmail.com>
2021-01-18 03:21:08 +0900

    install Prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,8 @@ import React, { ReactElement } from "react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
-const App = ({ Component, pageProps }: AppProps): ReactElement =>
-  <Component {...pageProps} />;
+const App = ({ Component, pageProps }: AppProps): ReactElement => (
+  <Component {...pageProps} />
+);
 
 export default App;

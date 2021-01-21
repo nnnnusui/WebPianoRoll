f5b3d53993044d92930e5c4ce4277d569948b7c4
nnnnusui <nnnnusui@gmail.com>
2021-01-22 03:08:49 +0900

    add: ontouchmove.preventDefault()

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
-const App = ({ Component, pageProps }: AppProps): ReactElement => (
-  <Component {...pageProps} />
-);
+const App = ({ Component, pageProps }: AppProps): ReactElement => {
+  useEffect(()=> {
+    window.addEventListener('touchmove', (e: TouchEvent) => e.preventDefault(), {passive: false});
+  }, [])
+  return <Component {...pageProps} />
+};
 
 export default App;

f2f389f5a156ea4b7a78ef4eac82050772f737ba
nnnnusui <nnnnusui@gmail.com>
2021-01-31 03:35:07 +0900

    add: ActionLayer to entity.Roll

--- src/components/pianoroll/entity/Roll.tsx
+++ src/components/pianoroll/entity/Roll.tsx
@@ -1,13 +1,26 @@
 import React from "react";
+import ActionLayer from "../grid/layer/ActionLayer";
 
 type Props = {
   id: number;
   division: number;
 };
 const Roll: React.FC<Props> = ({ id, division }) => {
+  const maxOffset = division;
+  const minOctave = 0;
+  const maxOctave = 0;
+  const maxPitch = 12;
+
+  const octaveRange = maxOctave + 1 - minOctave;
+  const height = octaveRange * maxPitch;
+  const width = maxOffset;
+
   return (
-    <div>
-      roll {id} _ {division}
+    <div className="relative h-full w-full">
+      <h1 className="absolute z-50">
+        __roll {id} _ {division}
+      </h1>
+      <ActionLayer {...{ width, height }} />
     </div>
   );
 };
--- src/components/pianoroll/grid/layer/ActionLayer.tsx
+++ src/components/pianoroll/grid/layer/ActionLayer.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { range0to } from "../../../range";
 import ActionCell from "../cell/ActionCell";
-import Grid from "../../contexts/GridContext";
 
-const ActionLayer: React.FC = () => {
-  const [{ width, height }] = [Grid.State()];
+type Props = {
+  width: number;
+  height: number;
+};
+const ActionLayer: React.FC<Props> = ({ width, height }) => {
   const style = {
     gridTemplateColumns: `repeat(${width}, 1fr)`,
     gridTemplateRows: `repeat(${height}, 1fr)`,

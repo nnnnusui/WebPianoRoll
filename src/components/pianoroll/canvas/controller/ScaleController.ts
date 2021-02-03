60bd4b52720b78e93baeb4286e4829519b8df7f5
nnnnusui <nnnnusui@gmail.com>
2021-02-04 00:36:41 +0900

    fix; ScaleController use `maxCount` arg

--- src/components/pianoroll/canvas/controller/ScaleController.ts
+++ src/components/pianoroll/canvas/controller/ScaleController.ts
@@ -3,20 +3,20 @@ import { Pos } from "../type/Pos";
 import { useState } from "react";
 
 const ScaleController = (maxPos: Pos, maxCount: number, step: number) => {
-  const [scaleCount, setScaleCount] = useState(0);
-  const scale = 1 + scaleCount * step;
+  const minCount = 0;
+  const [count, setCount] = useState(minCount);
+  const scale = 1 + count * step;
   const move = MoveController(maxPos, scale);
   const setScale = (scaleIn: boolean, viewLocal: Pos) => {
     const direction = scaleIn ? 1 : -1;
-    setScaleCount((prev) => {
+    setCount((prev) => {
       const next = prev + direction;
-      const result = scaleIn ? (next > 10 ? 10 : next) : next < 0 ? 0 : next;
+      const result = Math.max(Math.min(next, maxCount), minCount);
 
       const scaled = next == result;
-      const mouse = viewLocal;
       const ratio = {
-        width: (move.state.x + mouse.x) / (maxPos.x * scale),
-        height: (move.state.y + mouse.y) / (maxPos.y * scale),
+        width: (move.state.x + viewLocal.x) / (maxPos.x * scale),
+        height: (move.state.y + viewLocal.y) / (maxPos.y * scale),
       };
       const scalingVector = {
         x: step * maxPos.x,

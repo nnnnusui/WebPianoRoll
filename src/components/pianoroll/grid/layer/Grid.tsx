4b65720f5e29b49944cd17c4c9bc27dcb07db914
nnnnusui <nnnnusui@gmail.com>
2021-02-03 17:39:38 +0900

    add: ScaleController

--- src/components/pianoroll/grid/layer/Grid.tsx
+++ src/components/pianoroll/grid/layer/Grid.tsx
@@ -15,12 +15,12 @@ const Grid: React.FC<Props> = () => {
   const [canvasSize, setCanvasSize] = useState({ width: 100, height: 100 });
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const [scaleCount, setScaleCount] = useState(0);
-  const scale = scaleInfo.min + scaleCount * scaleInfo.step;
-  const move = MoveController(
+  const scale = ScaleController(
     { x: canvasSize.width, y: canvasSize.height },
-    scale
+    10,
+    0.5
   );
+  const move = scale.move;
 
   const canvas = canvasRef.current;
   const context = canvas?.getContext("2d");
@@ -31,8 +31,8 @@ const Grid: React.FC<Props> = () => {
     if (context == null) return;
     if (roll == null) return;
     const cellSize = {
-      width: (canvas.width / roll.width) * scale,
-      height: (canvas.height / roll.height) * scale,
+      width: (canvas.width / roll.width) * scale.state,
+      height: (canvas.height / roll.height) * scale.state,
     };
 
     const draw = () => {
@@ -70,40 +70,8 @@ const Grid: React.FC<Props> = () => {
     };
     const onWheel = (event: React.WheelEvent) => {
       const scaleIn = event.deltaY > 0;
-      setScaleCount((prev) => {
-        const direction = scaleIn ? 1 : -1;
-        const next = prev + direction;
-        const result = scaleIn ? (next > 10 ? 10 : next) : next < 0 ? 0 : next;
-
-        const scaled = next == result;
-        const mouse = getElementLocalMousePosFromEvent(event);
-        const ratio = {
-          width: (move.state.x + mouse.x) / (canvas.width * scale),
-          height: (move.state.y + mouse.y) / (canvas.height * scale),
-        };
-        const scalingVector = {
-          x: scaleInfo.step * canvas.width,
-          y: scaleInfo.step * canvas.height,
-        };
-        const moveVector = {
-          x: ratio.width * scalingVector.x * direction,
-          y: ratio.height * scalingVector.y * direction,
-        };
-        if (scaled) {
-          const scale = scaleInfo.min + result * scaleInfo.step;
-          move.setState((prev) => {
-            return {
-              scale: scale,
-              pos: {
-                x: prev.x + moveVector.x,
-                y: prev.y + moveVector.y,
-              },
-            };
-          });
-        }
-
-        return result;
-      });
+      const mouse = getElementLocalMousePosFromEvent(event);
+      scale.set(scaleIn, mouse);
     };
     return (
       <div
@@ -127,6 +95,51 @@ const Grid: React.FC<Props> = () => {
 };
 export default Grid;
 
+const ScaleController = (maxPos: Pos, maxCount: number, step: number) => {
+  const [scaleCount, setScaleCount] = useState(0);
+  const scale = 1 + scaleCount * step;
+  const move = MoveController(maxPos, scale);
+  const setScale = (scaleIn: boolean, viewLocal: Pos) => {
+    const direction = scaleIn ? 1 : -1;
+    setScaleCount((prev) => {
+      const next = prev + direction;
+      const result = scaleIn ? (next > 10 ? 10 : next) : next < 0 ? 0 : next;
+
+      const scaled = next == result;
+      const mouse = viewLocal;
+      const ratio = {
+        width: (move.state.x + mouse.x) / (maxPos.x * scale),
+        height: (move.state.y + mouse.y) / (maxPos.y * scale),
+      };
+      const scalingVector = {
+        x: scaleInfo.step * maxPos.x,
+        y: scaleInfo.step * maxPos.y,
+      };
+      const moveVector = {
+        x: ratio.width * scalingVector.x * direction,
+        y: ratio.height * scalingVector.y * direction,
+      };
+      if (scaled) {
+        const scale = scaleInfo.min + result * scaleInfo.step;
+        move.setState((prev) => {
+          return {
+            scale: scale,
+            pos: {
+              x: prev.x + moveVector.x,
+              y: prev.y + moveVector.y,
+            },
+          };
+        });
+      }
+      return result;
+    });
+  };
+  return {
+    set: setScale,
+    state: scale,
+    move: move,
+  };
+};
 const MoveController = (max: Pos, scale: number) => {
   const [onMove, setOnMove] = useState(false);
   const [from, setFrom] = useState({ x: 0, y: 0 });

3035cb17a7bcc912025c33593d3afc10601a9cc1
nnnnusui <nnnnusui@gmail.com>
2021-02-05 02:24:42 +0900

    fix: remove roll in view

--- src/components/pianoroll/View.tsx
+++ src/components/pianoroll/View.tsx
@@ -1,23 +1,12 @@
-import React, { useState, useRef } from "react";
-import Context from "./context/Context";
-import Roll from "./entity/Roll";
-import ActionLayer from "./grid/layer/ActionLayer";
+import React from "react";
 import ActionListeners from "./grid/ActionListeners";
-import ActionConsumer from "./ActionConsumer";
 import Grid from "./canvas/Grid";
-import Select from "./grid/layer/Select";
 
 const View: React.FC = () => {
-  const selectedRollId = Context.roll.selectedId.State();
-  const roll = Context.rolls.State().get(selectedRollId);
   const actionListeners = ActionListeners();
-  if (roll == null) return <></>;
 
   return (
     <div className="relative h-full w-full" {...actionListeners}>
-      {/* <ActionConsumer {...roll.data} />
-      <ActionLayer {...roll.data} />
-      <Roll {...roll.data} /> */}
       <div className="absolute h-full w-full">
         <Grid />
       </div>

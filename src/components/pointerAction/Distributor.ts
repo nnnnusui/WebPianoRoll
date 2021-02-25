2b9c25bf2fc60fd2b297e6c0d05f718048ed5d93
nnnnusui <nnnnusui@gmail.com>
2021-02-25 15:01:36 +0900

    update: condition expr

--- src/components/pointerAction/Distributor.ts
+++ src/components/pointerAction/Distributor.ts
@@ -12,28 +12,28 @@ type Executor = {
 };
 type ExecutorFactory = (from: From) => (to: To) => Executor;
 type Condition = (from: From) => (to: To) => boolean;
-type Node = ExecutorFactory;
 const DummyExecutor: ExecutorFactory = () => () => ({
   tryExecute: () => {},
   execute: () => {},
   cancel: () => {},
 });
-const TreeBuilder = (condition: Condition) => (
-  onTrue: Node = DummyExecutor,
-  onFalse: Node = DummyExecutor
-) => (from: Event) => (to: Event) =>
-  condition(from)(to) ? onTrue(from)(to) : onFalse(from)(to);
+const Conditions = (pointers: State) => {
+  return {
+    hold: (milliSec: number) => (from: Event) => (to: Event) => {
+      const difference = to.timeStamp - from.timeStamp;
+      return milliSec > difference;
+    },
+  };
+};
 
 const Log = (name: string): ExecutorFactory => () => () => ({
   tryExecute: () => console.log(`tryExecute: ${name}`),
   execute: () => console.log(`executed: ${name}`),
   cancel: () => console.log(`canceled: ${name}`),
 });
-const Distance = (length: number): Condition => (from: Event) => (
-  to: Event
-) => {
+const Distance = (upper: number): Condition => (from: Event) => (to: Event) => {
   const distance = Math.abs(to.clientX - from.clientX);
-  return length <= distance;
+  return upper > distance;
 };
 const Degree = (upper: number): Condition => (from: Event) => (to: Event) => {
   const radian = Math.atan2(
@@ -41,27 +41,23 @@ const Degree = (upper: number): Condition => (from: Event) => (to: Event) => {
     to.clientY - from.clientY
   );
   const degree = 360 - (radian * (180 / Math.PI) + 180);
-  return upper >= degree;
+  return upper > degree;
 };
-const RadialMenu = (factories: ExecutorFactory[]) => (from: Event) => (to: Event) => {
-  const division = factories.length
-  const unit = 360 / division
-  const shift = unit / 2
-  const conditions = factories
-    .map((factory, index) => ({
-      degree: Degree(unit * index + shift),
-      factory
-    }))
-  const head = conditions[0] || {degree: Degree(360), factory: DummyExecutor}
-  const target = conditions.find(({degree}) => degree(from)(to)) || head
+const RadialMenu = (factories: ExecutorFactory[]) => (from: Event) => (
+  to: Event
+) => {
+  const division = factories.length;
+  const unit = 360 / division;
+  const shift = unit / 2;
+  const conditions = factories.map((factory, index) => ({
+    degree: Degree(unit * index + shift),
+    factory,
+  }));
+  const head = conditions[0] || { degree: Degree(360), factory: DummyExecutor };
+  const target = conditions.find(({ degree }) => degree(from)(to)) || head;
   return target.factory(from)(to);
 };
-const Tree = RadialMenu([
-  Log("up"),
-  Log("right"),
-  Log("down"),
-  Log("left"),
-]);
+const Tree = RadialMenu([Log("up"), Log("right"), Log("down"), Log("left")]);
 
 const State = () => {
   return useIdMapState<{ tree: (to: Event) => Executor }>();
